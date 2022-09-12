f72e4426778d06ed4facf9df015a220ffaa20528
NVH736 <nguyenhuuvqht@gmail.com>
2022-09-13 02:05:17 +0700

    NVH_0736

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -15,10 +15,19 @@ public class EmployeeController : Controller
 
     public IActionResult Index()
     {
+        List<Employee> stdList = new List<Employee>()
+        {
+            new Employee {EmployeeID = 1, EmployeeName = "Nguyen Van A", EmployeeAge = 18},
+            new Employee {EmployeeID = 2, EmployeeName = "Nguyen Van B", EmployeeAge = 19},
+            new Employee {EmployeeID = 3, EmployeeName = "Nguyen Van C", EmployeeAge = 25},
+            new Employee {EmployeeID = 4, EmployeeName = "Nguyen Van D", EmployeeAge = 20},
+            new Employee {EmployeeID = 5, EmployeeName = "Nguyen Van E", EmployeeAge = 26},
+        };
+        ViewData["Employees"] = stdList;
         return View();
     }
-
-    public IActionResult Privacy()
+    [HttpPost]
+    public IActionResult Create(Employee std)
     {
         return View();
     }
