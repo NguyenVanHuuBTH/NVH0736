dbf496d01384d25e03f4ea3675dd5bebdce1d683
NVH736 <nguyenhuuvqht@gmail.com>
2022-10-05 16:51:02 +0700

    NVH_0736

---
+++ NguyenVanHuuBTH2/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenVanHuuBTH2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
